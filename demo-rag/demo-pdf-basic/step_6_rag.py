1ba4c01dddd4b6f9047889026f9117ff4ed18f16
up1 <somkiat.p@gmail.com>
2025-07-13 11:45:58 +0700

    RAG with OpenAI

---
+++ demo-rag/demo-pdf-basic/step_6_rag.py
@@ -0,0 +1,44 @@
+from openai import OpenAI
+from step_5_retrieve import retrieve_from_vectordb
+
+openai_client = OpenAI()
+
+def rag(query, retrieved_documents, model="gpt-4.1"):
+    information = "\n\n".join(retrieved_documents)
+
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful expert financial research assistant. Your users are asking questions about information contained in an annual report."
+            "You will be shown the user's question, and the relevant information from the annual report. Answer the user's question using only this information."
+        },
+        {"role": "user", "content": f"Question: {query}. \n Information: {information}"}
+    ]
+    
+    response = openai_client.chat.completions.create(
+        model=model,
+        messages=messages,
+    )
+    content = response.choices[0].message.content
+    return content
+
+if __name__ == "__main__":
+    query = "What was the total revenue of 2022, 2021 and 2020?"
+
+    # Retrieve documents from the database
+    docs = retrieve_from_vectordb(query)
+    # join the documents and distances into a single string
+
+    print("Retrieved Documents:")
+    for document, distance in zip(docs['documents'], docs['distances']):
+        print("======================")
+        print("Retrieved Document:")
+        print(document)
+        print("Distance:")
+        print(distance)
+        print('\n')
+    
+    # Use RAG to answer the question
+    answer = rag(query, docs['documents'])
+    print("Question:", query)
+    print("Answer:", answer)
